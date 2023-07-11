bdb22e5c0c6a68ddf5db85f8619f13d38e501412
Nagy Ádám <madar555@gmail.com>
2023-07-11 06:28:18 +0200

    favicon,logo,szoveg modositasa

--- vue3-gyorstalpalo/src/App.vue
+++ vue3-gyorstalpalo/src/App.vue
@@ -5,13 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="../public/favicon.jpg" class="logo" alt="AL logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Nagyi" />
 </template>
 
 <style scoped>
